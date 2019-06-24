73569f649bda30b68c3dc03167091ccdd33b4bdd
ahueni <ahueni@geo.uzh.ch>
2019-06-24 17:19:30 +0200

    New method to create a new user with the current client connection (admin only)

--- src/webapp/src/ch/specchio/services/UserService.java
+++ src/webapp/src/ch/specchio/services/UserService.java
@@ -4,9 +4,12 @@ import javax.annotation.security.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
+import au.ands.org.researchdata.ResearchDataAustralia;
+import au.ands.org.researchdata.SpectralLibrary;
 import ch.specchio.constants.UserRoles;
 import ch.specchio.factories.SPECCHIOFactoryException;
 import ch.specchio.factories.UserFactory;
+import ch.specchio.services.BadRequestException;
 import ch.specchio.types.User;
 
 
@@ -17,6 +20,65 @@ import ch.specchio.types.User;
 @DeclareRoles({UserRoles.ADMIN, UserRoles.USER})
 public class UserService extends SPECCHIOService {
 	
+	
+	
+	/**
+	 * Create a new user account on this service: only possible for admin users already logged into this server and database
+	 * 
+	 * @param user	an object describing the user to be created
+	 *
+	 * @throws SecurityException		a non-admin user tried to create an admin user
+	 * @throws SPECCHIOFactoryException	database error
+	 * 
+	 * @return a user object containing the complete details of the account
+	 */
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("createUserAccount")
+	public User createUserAccount(User user) throws SPECCHIOFactoryException {
+		
+		
+		UserFactory factory = new UserFactory(getClientUsername(), getClientPassword(), getDataSourceName(), isAdmin());
+
+		if (!getSecurityContext().isUserInRole(UserRoles.ADMIN)) {
+			// a non-admin user is trying to create a user on this system
+			throw new SecurityException("Non-admin users can not create users. Nice try, guys.");
+		}
+
+		if (user.isInRole(UserRoles.ADMIN)) {
+			// an anonymous user is trying to create an admin user
+			throw new SecurityException("Administrator accounts cannot be created using this service.");
+		}
+		
+		// create a user factory to do the work
+//		UserFactory factory = new UserFactory(getDataSourceName());
+		try {
+		
+			// create the account
+			factory.insertUser(user);
+			
+			if (getServerCapability(ResearchDataAustralia.ANDS_SERVER_CAPABILITY) != null) {
+				// make sure the user has an ANDS party identifier
+				if (user.getExternalId() == null || user.getExternalId().length() == 0) {
+					user.setExternalId(ResearchDataAustralia.generatePartyIdentifier(SpectralLibrary.PARTY_ID_PREFIX, user));
+					factory.updateUser(user);
+				}
+			}
+			
+		}
+		catch (IllegalArgumentException ex) {
+			// illegal data in the user object
+			throw new BadRequestException(ex);
+		}
+		finally {
+			// clean up
+			factory.dispose();
+		}
+		
+		return user;
+		
+	}	
 	
 	/**
 	 * List all of the users in the database with user statistics added.
