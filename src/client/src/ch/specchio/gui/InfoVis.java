f9408bd14b0bce2e523c77fcf96cfa1163522ea0
Bastian Buman <bastian.buman@geo.uzh.ch>
2020-01-29 11:14:52 +0100

    This class serves as an interactive data visualization and analysis tool. Much like Kibana or Tableau, the visualization will try to adapt to the provided data. Creating interesting visualizations. The interactivity includes filtering, zooming, brushing and linking, machine learning techniques, data analysis techniques, data manipulation techniques and data export techniques.

---
+++ src/client/src/ch/specchio/gui/InfoVis.java
@@ -0,0 +1,24 @@
+package ch.specchio.gui;
+
+
+// This class serves as an interactive data visualization and analysis tool. Much like Kibana or Tableau, the visualization
+// will try to adapt to the provided data. Creating interesting visualizations. The interactivity includes filtering,
+// zooming, brushing and linking, machine learning techniques, data analysis techniques, data manipulation techniques
+// and data export techniques.
+public class InfoVis {
+    // Include functionality from SpectralDataBrowser
+    // Include a data selection based on range sliders and other available info, which get their values from asking the DB about min and max or possible values (e.g. if strings)
+    // Include drag and drop of data from selection onto visualization pane
+    // Include check the hierarchical level of the dropped data. Split visualizations per unit and sensor
+    // Include band selection if not the whole spectrum is of interest
+    // Include interactive data selection - brushing and linking, zooming, parallel coordinates, box plot, violin plot,
+    // Include metadata information visualization and or presentation
+    // Include color visualizations per campaign - allows to compare campaigns
+    // Include data manipulation - normalization
+    // Include data analyses - machine learning, pca, mds, etc.
+    // Include data export - selection of data as CSV
+    // Include data export comes with a report - selection criteria (e.g. campaign(s), researcher(s), metadata selections)
+    // Include data amount checker - if too many spectra are selected, a warning pops-up, that it might take a long time to load. ...
+    // Then start by showing a selection of the data while the other data loads in the background.
+
+}
