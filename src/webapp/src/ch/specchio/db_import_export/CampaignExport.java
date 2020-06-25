7737349e50f3de14d03134a14eae5024c91dd407
ahueni <ahueni@geo.uzh.ch>
2020-06-25 11:39:26 +0200

    Adding threading to export campaign to maybe deal with interrupted writing experienced by GA

--- src/webapp/src/ch/specchio/db_import_export/CampaignExport.java
+++ src/webapp/src/ch/specchio/db_import_export/CampaignExport.java
@@ -3,6 +3,14 @@ package ch.specchio.db_import_export;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import ch.specchio.constants.FileTypes;
 import ch.specchio.eav_db.SQL_StatementBuilder;
@@ -30,21 +38,51 @@ public class CampaignExport extends DbStructure {
 	
 	public void export(OutputStream os) throws IOException
 	{
-		// create writer
-		TableWriterFactory factory = new TableWriterFactory();
-		w = factory.getWriter(os, FileTypes.CAMPAIGN_EXPORT_XML);
-		
-		w.write("<campaign>"); // proper xml document ....
-		w.write_nl();
 
-		DbTable campaign_dbt = get_table("campaign");  
-		campaign_dbt.export(campaign_id, null); // start the export with the campaign, the rest is recursive
-			
-		w.write("</campaign>"); // proper xml document ....
+
+		ThreadPoolExecutor exec = (ThreadPoolExecutor) Executors
+				.newCachedThreadPool();
+
+		Runnable  task = new Runnable()  {
+
+			@Override
+			public void run() {
+				try {
+
+					// create writer
+					TableWriterFactory factory = new TableWriterFactory();
+					w = factory.getWriter(os, FileTypes.CAMPAIGN_EXPORT_XML);
+
+					w.write("<campaign>"); // proper xml document ....
+					w.write_nl();
+
+					DbTable campaign_dbt = get_table("campaign");  
+					campaign_dbt.export(campaign_id, null); // start the export with the campaign, the rest is recursive
+
+					w.write("</campaign>"); // proper xml document ....
+
+					w.close();	            	
+
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+
+			}
+		};
+
+		exec.execute(task);
 		
-		w.close();
+		try {
+			exec.awaitTermination(1, TimeUnit.HOURS);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 
 	}
-	
+
 
 }
