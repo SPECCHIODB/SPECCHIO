6935f2806dfb7e75a951266af8d4e68cf2507f95
Eric Hay <55721785+EricHay@users.noreply.github.com>
2021-01-22 09:05:20 +1100

    Update OceanView_FileLoader.java

--- src/client/src/ch/specchio/file/reader/spectrum/OceanView_FileLoader.java
+++ src/client/src/ch/specchio/file/reader/spectrum/OceanView_FileLoader.java
@@ -91,7 +91,7 @@ public class OceanView_FileLoader extends JAZ_FileLoader{
 	{		
 		String[] sub_tokens = tokens[1].split(" ");
 		
-		MetaParameter mp = MetaParameter.newInstance(attributes_name_hash.get("Number of internal Scans"));
+		MetaParameter mp = MetaParameter.newInstance(attributes_name_hash.get("Number of Internal Scans"));
 		try {
 			mp.setValue(Integer.valueOf(sub_tokens[0]), "RAW");
 			smd.addEntry(mp);
