dc8dff9f0b47de085fa6588537eb625ea1fbdd79
ahueni <ahueni@geo.uzh.ch>
2024-06-10 15:43:09 +0200

    Added the JPL laser as pseudo line lamp

--- conf/mysql/specchio_database_upgrade_V3.3.6_V3.3.7.sql
+++ conf/mysql/specchio_database_upgrade_V3.3.6_V3.3.7.sql
@@ -6,6 +6,8 @@ INSERT INTO `specchio`.`attribute`(`name`, `category_id`, `default_storage_field
 
 INSERT INTO `specchio`.`measurement_unit` (`name`, `ASD_Coding`) VALUES ('AOT',102);
 
+INSERT INTO `specchio`.`taxonomy` (`attribute_id`, `name`, `code`, `description`) VALUES ((select `attribute_id` from `specchio`.`attribute` where name = 'Spectral Line Lamp'), 'JPL Laser', 'JPL laser emissions', '');
+
 
 -- db version
 INSERT INTO `specchio`.`schema_info` (`version`, `date`) VALUES ('3.37', CURDATE());
